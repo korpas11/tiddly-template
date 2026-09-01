e4cd55f70cd8d451dda462e7894809008d130ae2
korpas11 <korpas11@67hit.com>
2026-09-01 14:35:53 -0500

    Build script

---
+++ build-static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export TIDDLYWIKI_PLUGIN_PATH=$SCRIPT_DIR/plugins
+export TIDDLYWIKI_THEME_PATH=$SCRIPT_DIR/plugins
+
+CONFIG_FILE="$SCRIPT_DIR/config.sh"
+
+if [[ -f "$CONFIG_FILE" ]]; then
+    source "$CONFIG_FILE"
+else
+    echo "Config file not found: $CONFIG_FILE" >&2
+    exit 1
+fi
+
+WIKIDIR=$SCRIPT_DIR/content
+LOG_DIR=$SCRIPT_DIR/log
+ACCESS_LOG=$LOG_DIR/access.log
+ERROR_LOG=$LOG_DIR/error.log
+WIKI_PID=$LOG_DIR/wiki.pid
+
+cd $SCRIPT_DIR
+
+$WIKIEXE $WIKIDIR --build static
+
